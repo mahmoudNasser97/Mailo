ed498ce6f8462486a82e8efd4ffe7a3a302e1087
Nasser <mahmoudnasser820@gmail.com>
2026-07-20 18:18:48 +0300

    feat: add NPCHitReaction subscribing to HitReactor.OnImpact

---
+++ Mailo/Assets/Mahmoud SandBox/NPC/NPCHitReaction.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(NPCBrain))]
+public class NPCHitReaction : MonoBehaviour
+{
+    [SerializeField] float _hitThreshold       = 20f;
+    [SerializeField] float _knockdownThreshold = 80f;
+    [SerializeField] float _hitRecoverTime     = 0.8f;
+
+    static readonly int _hitReactHash = Animator.StringToHash("HitReact");
+
+    NPCBrain  _brain;
+    Animator  _animator;
+    HitReactor _hitReactor;
+
+    void Awake()
+    {
+        _brain    = GetComponent<NPCBrain>();
+        _animator = GetComponent<Animator>();
+
+        _hitReactor = GetComponentInParent<HitReactor>();
+        if (_hitReactor == null)
+            _hitReactor = transform.root.GetComponentInChildren<HitReactor>();
+    }
+
+    void OnEnable()
+    {
+        if (_hitReactor != null) _hitReactor.OnImpact += HandleImpact;
+    }
+
+    void OnDisable()
+    {
+        if (_hitReactor != null) _hitReactor.OnImpact -= HandleImpact;
+    }
+
+    void HandleImpact(float impulse, Vector3 direction, Vector3 hitPoint)
+    {
+        if (impulse < _hitThreshold)       return; // too small to react
+        if (impulse >= _knockdownThreshold) return; // ragdoll handles heavy hits
+        if (_brain.State == NPCState.HitReact) return; // already in hit react
+
+        _brain.ReportHit(impulse, hitPoint, direction);
+        _animator.SetTrigger(_hitReactHash);
+        StartCoroutine(RecoverAfter(_hitRecoverTime));
+    }
+
+    IEnumerator RecoverAfter(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        _brain.RecoverFromHit();
+    }
+}
